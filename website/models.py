d808ee786c7f1c3f8d26ecd0b52a17a4c8e4860f
Patrick-Bonini <patrick.bonini13@gmail.com>
2023-05-19 08:01:22 -0400

    database changes

--- website/models.py
+++ website/models.py
@@ -6,9 +6,11 @@ class User_Info(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     monthlyIncome = database.Column(database.Integer)
     date = database.Column(database.DateTime(timezone=True), default=func.now())
+    user_id = database.Column(database.Integer, database.ForignKey('user.id'))
 
 class User(database.Model, UserMixin):
     id = database.Column(database.Integer, primary_key=True)
     email = database.Column(database.String(75), unique=True)
     password = database.Column(database.String(30))
     firstName = database.Column(database.String(30))
+    user_info = database.relationship('User_Info')
